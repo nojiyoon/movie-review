335444f89ce322d58cfb6d26b215f5045795de0a
nojiyoon <neutral.sth@gmail.com>
2022-04-06 11:19:51 +0900

    0406 final

--- pred/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Review(models.Model):
-    title = models.CharField(max_length=200)
-    review = models.CharField(max_length=200)
-    score = models.CharField(max_length=3)
--- pred/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PredView
-from .views import IndexView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('api/test/', PredView.as_view()),  #TODO API로 사용 안할거면 View 합쳐도 될듯 ㅎ
-]
